3c881521dc97dcec88f0fb9ce3aaf9e1a1ae1cc9
Maurilio Santana <cabelotaina@gmail.com>
2019-08-17 23:39:47 -0300

    update twitter intent

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  twitter: 'https://twitter.com/intent/tweet?text=N%C3%A3o%20acredito%20que%20vamos%20deixar%20a%20escolha%20do%20embaixador%20brasileiro%20nos%20EUA%20ser%20crit%C3%A9rio%20pessoal%20de%20Bolsonaro!%20%23RespeitaoItamaraty%20%0A%0AChega%20de%20nepotismo!%20Queremos%20que%20o%20Itamaraty%20e%20o%20Brasil%20sejam%20respeitados!%20N%C3%A3o%20para%20Eduardo%20Bolsonaro%20na%20Embaixada!&hashtags=RespeitaoItamaraty'
+  twitter: 'https://twitter.com/intent/tweet?text=N%C3%A3o%20acredito%20que%20vamos%20deixar%20a%20escolha%20do%20embaixador%20brasileiro%20nos%20EUA%20ser%20crit%C3%A9rio%20pessoal%20de%20Bolsonaro!%20%23RespeitaoItamaraty%20%0A%0AChega%20de%20nepotismo!%20Queremos%20que%20o%20Itamaraty%20e%20o%20Brasil%20sejam%20respeitados!%20N%C3%A3o%20para%20Eduardo%20Bolsonaro%20na%20Embaixada!'
 };
